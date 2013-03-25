df3a5d8443d03731d7922395e8e543c1ec772140
loosethisskin <loosethisskin@gmail.com>
2013-03-25 18:22:15 -0400

    otter: add ramdisk and bootimg scripts

---
+++ otter/make-ramdisk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ramdisk
+# make sure all directories are created because git doesn't save empty directories
+mkdir -p data dev proc sbin sys system
+chmod 750 init*
+chmod 750 sbin/adbd
+chmod 644 default.prop
+chmod 640 fstab.$DEVICE
+chmod 644 ueventd*
+find . | cpio -o -H newc | gzip > ../ramdisk.cpio.gz
