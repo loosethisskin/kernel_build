e3271b9c74aec3443436f23b7f7c1da2bc305474
loosethisskin <loosethisskin@gmail.com>
2014-05-19 19:32:09 -0400

    otter: Follow kernel cmdline

--- otter/make-bootimg.sh
+++ otter/make-bootimg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkbootimg --kernel zImage --cmdline 'mem=512M console=/dev/null androidboot.selinux=permissive' --base 0x80000000 --pagesize 4096 --ramdisk ramdisk.cpio.gz --output boot.img
+mkbootimg --kernel zImage --cmdline 'mem=512M console=null androidboot.selinux=permissive' --base 0x80000000 --pagesize 4096 --ramdisk ramdisk.cpio.gz --output boot.img
 
 exit $?
