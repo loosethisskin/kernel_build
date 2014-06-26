6a2092bb07d4d2baedbcd37c6f78488cafcadd5d
loosethisskin <loosethisskin@gmail.com>
2014-06-25 22:35:05 -0400

    p4wifi: CM11 Changes

--- p4wifi/make-ramdisk.sh
+++ p4wifi/make-ramdisk.sh
@@ -5,12 +5,23 @@ cp -r $BUILD_ROOT_DIR/$DEVICE/ramdisk $TARGET_DIR/ramdisk
 
 cd $TARGET_DIR/ramdisk
 
+rm fstab.p3*
+if [ "$F2FS_BUILD" == true ]; then
+  echo cp $BUILD_ROOT_DIR/$DEVICE/ramdisk/fstab.p3-f2fs fstab.p3
+  cp $BUILD_ROOT_DIR/$DEVICE/ramdisk/fstab.p3-f2fs fstab.p3
+else
+  echo cp $BUILD_ROOT_DIR/$DEVICE/ramdisk/fstab.p3 fstab.p3
+  cp $BUILD_ROOT_DIR/$DEVICE/ramdisk/fstab.p3 fstab.p3
+fi
+
 # make sure all directories are created because git doesn't save empty directories
 mkdir -p data dev proc sbin sys system
 chmod 750 init*
 chmod 750 sbin/adbd
+chmod 750 sbin/healthd
 chmod 644 default.prop
 chmod 640 fstab.p3
 chmod 644 ueventd*
+chmod 644 property_contexts file_contexts seapp_contexts sepolicy
 chmod 644 lpm.rc
 find . | cpio -o -H newc | gzip > ../ramdisk.cpio.gz
