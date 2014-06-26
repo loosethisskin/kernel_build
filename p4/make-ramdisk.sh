01dfefd6deee88bffe02930088ce9846a6ee22a6
loosethisskin <loosethisskin@gmail.com>
2014-06-25 22:35:19 -0400

    p4: CM11 Changes

--- p4/make-ramdisk.sh
+++ p4/make-ramdisk.sh
@@ -9,9 +9,11 @@ cd $TARGET_DIR/ramdisk
 mkdir -p data dev proc sbin sys system
 chmod 750 init*
 chmod 750 sbin/adbd
+chmod 750 sbin/healthd
 chmod 750 sbin/cbd
 chmod 644 default.prop
 chmod 640 fstab.p3
 chmod 644 ueventd*
+chmod 644 property_contexts file_contexts seapp_contexts sepolicy
 chmod 644 lpm.rc
 find . | cpio -o -H newc | gzip > ../ramdisk.cpio.gz
